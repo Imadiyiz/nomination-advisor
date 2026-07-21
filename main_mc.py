c61ecd8a0a4d7d6e873c79c1aca9e51745981d39
imadiyiz <michaelimadiyi@yahoo.com>
2026-07-21 11:36:17 +0100

    add move score and true win percentage

--- main_mc.py
+++ main_mc.py
@@ -4,6 +4,24 @@ from typing import Dict, Set
 from collections import defaultdict
 
 
+# basic reward weighting which gives a maximum reward for equalling bid
+# rewards decreases as the difference between the bid and actual score increases
+# Slightly favours overestimating the bid compared to unachieving bid
+reward_weighting = {
+   -1: 0.1,
+    0: 1,
+    1: 0.15,
+    2: 0.2,
+    3: 0.25,
+    4: 0.4,
+    5: 0.5,
+    6: 0.65,
+    7: 0.8,
+    8: 0.95,
+
+}
+
+
 def run_monte_carlo(root_state: GameState, perspective: str, N_rollouts: int = 100):
     """
     Runs Monte Carlo evaluation for all legal moves of the current leader.
@@ -30,13 +48,11 @@ def run_monte_carlo(root_state: GameState, perspective: str, N_rollouts: int = 1
         perspective_player=perspective
     )
 
-
+    # move score, and true percentrage frequency
     total_score = 0
+    tp_freq = 0
 
     for _ in range(N_rollouts):
-
-        distributions = defaultdict(int)
-
 
         # Sample a possible world consistent with beliefs
         sampled_hands: Dict[str, Set[str]] = belief_model.sample_world()
@@ -67,23 +83,36 @@ def run_monte_carlo(root_state: GameState, perspective: str, N_rollouts: int = 1
         final_scores = simulator.rollout()
         result = final_scores[perspective]
 
-        total_score += result
+        # generate perspective bid
+        for i, v in root_state.bids.items():
+            if i == perspective:
+                perspective_bid = v
 
+        # if perspective wins sames amount as bids, score increases
+        bid_spread = result - perspective_bid  
+        if bid_spread in reward_weighting:
+            total_score += reward_weighting[bid_spread]
 
+        # also generate true percentage
+        if result == perspective_bid:
+            tp_freq += 1
+        
     # calculations
 
-    bidding_estimation = round(total_score / N_rollouts, 2) 
+    move_score = round(total_score / N_rollouts, 2) 
+    win_percentage = round(tp_freq / N_rollouts, 2) 
 
     # return context of distribution and expected tricks
     summary_context = {}
-    summary_context["expected_tricks"] = bidding_estimation 
+    summary_context["move_score"] = move_score 
+    summary_context["Win_percentage"] = win_percentage 
     return summary_context
 
 ########
 
 # Players
 players = ("A", "B", "C", "D")
-my_player = "D"
+my_player = "A"
 
 # Hands (sets of card initials)
 hands = (
@@ -97,10 +126,10 @@ root_state = GameState(
     hands=dict(hands),                 # Convert tuple pairs to dict
     current_trick=(),                  # No cards played yet
     leader=my_player,                  # A leads
-    trump_suit="D",                    # Hearts are trump
+    trump_suit="S",                    # Spades are trump
     player_order=players,
     round_scores={p: 0 for p in players},
-    bids={p: 1 for p in players},      # Arbitrary example bids
+    bids={p: 2 for p in players},      # Arbitrary example bids
     cards_remaining=8                  # 8 cards each
 )
 
@@ -124,6 +153,4 @@ for item in bidding_estimates.items():
 # it needs to work out who wins the current hand, not the full round
 
 
-"""
-
-"""
+# Write the whole logic flow on paper and see where the issues comes from
